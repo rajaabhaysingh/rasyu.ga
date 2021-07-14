fbf7c8450204e05dfd51ae82e6bd7a813c74302a
rajaabhaysingh <rajaabhaysingh233@gmail.com>
2021-07-14 16:02:20 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+// components or views
+import Header from "./components/Header";
+import Home from "./views/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>{/* <Header /> */}</header>
+      <Home />
     </div>
   );
 }
 
 export default App;
+
+// header - -you don't really need header-
+// main intro slide - 1
+// experience - 6 - {each experience linked to bottom of page with work ss - 11}
+// Projects - 8
+// education - 7
+// Skillsets - 10
+// ---- work experiences sshots ----
+// Hobbies & interests - 10
+// Contact info
+// Thank you
---
+++ src/views/Home/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div>Under maintenance... Will be back soon.</div>;
+};
+
+export default Home;
